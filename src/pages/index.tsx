c1026f4ec690000c2c8af1a12a6071482ada7b47
Sameer Choudhary <62991891+sameerkch12@users.noreply.github.com>
2025-11-20 15:09:21 +0530

    Refactor IndexPage to include DefaultLayout
    Moved the 'sameer' heading inside the DefaultLayout component.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,10 +10,14 @@ import DefaultLayout from "@/layouts/default";
 export default function IndexPage() {
   return (
     <>
+    <DefaultLayout>
+
+ <h1>sameer</h1>
+    </DefaultLayout>
  
   
 
-    <h1>sameer</h1>
+   
 
     
     
